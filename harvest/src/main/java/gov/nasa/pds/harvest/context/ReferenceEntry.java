10ea798c25ecc04a29abfd59fffa76c4fc291c72
mcayanan <mcayanan@45d5965c-88be-4637-9056-144873d6cd01>
2010-09-13 21:05:48 +0000

    Convert tabs to spaces
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@7742 45d5965c-88be-4637-9056-144873d6cd01

--- harvest/src/main/java/gov/nasa/pds/harvest/context/ReferenceEntry.java
+++ harvest/src/main/java/gov/nasa/pds/harvest/context/ReferenceEntry.java
@@ -1,56 +1,56 @@
 package gov.nasa.pds.harvest.context;
 
 public class ReferenceEntry {
-	private String logicalID;
-	private String version;
-	private String associationType;
-	private String objectType;
-	
-	private boolean hasVersion;
-	
-	public ReferenceEntry() {
-		logicalID = null;
-		version = null;
-		associationType = null;
-		objectType = null;
-		
-		hasVersion = false;
-	}
-	
-	public String getLogicalID() {
-		return logicalID;
-	}
-	
-	public void setLogicalID(String id) {
-		logicalID = id;
-	}
-	
-	public String getVersion() {
-		return version;
-	}
-	
-	public void setVersion(String ver) {
-		version = ver;
-		hasVersion = true;
-	}
-	
-	public boolean hasVersion() {
-		return hasVersion;
-	}
-	
-	public String getAssociationType() {
-		return associationType;
-	}
-	
-	public void setAssociationType(String type) {
-		associationType = type;
-	}
-	
-	public String getObjectType() {
-		return objectType;
-	}
-	
-	public void setObjectType(String type) {
-		objectType = type;
-	}
+    private String logicalID;
+    private String version;
+    private String associationType;
+    private String objectType;
+
+    private boolean hasVersion;
+
+    public ReferenceEntry() {
+        logicalID = null;
+        version = null;
+        associationType = null;
+        objectType = null;
+
+        hasVersion = false;
+    }
+
+    public String getLogicalID() {
+        return logicalID;
+    }
+
+    public void setLogicalID(String id) {
+        logicalID = id;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String ver) {
+        version = ver;
+        hasVersion = true;
+    }
+
+    public boolean hasVersion() {
+        return hasVersion;
+    }
+
+    public String getAssociationType() {
+        return associationType;
+    }
+
+    public void setAssociationType(String type) {
+        associationType = type;
+    }
+
+    public String getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(String type) {
+        objectType = type;
+    }
 }
