1f13ffe43492b17857080428b2936c23f5051059
pramirez <pramirez@45d5965c-88be-4637-9056-144873d6cd01>
2010-10-11 03:09:58 +0000

    Add in exception class for registry service.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@7826 45d5965c-88be-4637-9056-144873d6cd01

---
+++ registry/registry-service/src/main/java/gov/nasa/pds/registry/exception/RegistryServiceException.java
@@ -0,0 +1,46 @@
+//	Copyright 2009-2010, by the California Institute of Technology.
+//	ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+//	Any commercial use must be negotiated with the Office of Technology 
+//	Transfer at the California Institute of Technology.
+//	
+//	This software is subject to U. S. export control laws and regulations 
+//	(22 C.F.R. 120-130 and 15 C.F.R. 730-774). To the extent that the software 
+//	is subject to U.S. export control laws and regulations, the recipient has 
+//	the responsibility to obtain export licenses or other export authority as 
+//	may be required before exporting such information to foreign countries or 
+//	providing access to foreign nationals.
+//	
+//	$Id$
+//
+
+package gov.nasa.pds.registry.exception;
+
+/**
+ * @author pramirez
+ * 
+ */
+public class RegistryServiceException extends Exception {
+  private static final long serialVersionUID = -3676574693217741086L;
+  private final ExceptionType exceptionType;
+  private final String messageKey;
+  private final Object[] arguments;
+
+  public RegistryServiceException(final String messageKey,
+      final ExceptionType exceptionType, final Object... arguments) {
+    this.messageKey = messageKey;
+    this.exceptionType = exceptionType;
+    this.arguments = arguments;
+  }
+
+  public ExceptionType getExceptionType() {
+    return this.exceptionType;
+  }
+
+  public String getMessageKey() {
+    return this.messageKey;
+  }
+
+  public Object[] getArguments() {
+    return this.arguments;
+  }
+}
