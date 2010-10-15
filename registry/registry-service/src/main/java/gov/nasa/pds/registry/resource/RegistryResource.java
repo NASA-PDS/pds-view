385dfdd78521906c32a42d6fcbb722c3696a8fea
pramirez <pramirez@45d5965c-88be-4637-9056-144873d6cd01>
2010-10-15 19:23:25 +0000

    Support of routing of requests for AuditableEvents, ClassificationSchemes, Services, and Classifications.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@7881 45d5965c-88be-4637-9056-144873d6cd01

--- registry/registry-service/src/main/java/gov/nasa/pds/registry/resource/RegistryResource.java
+++ registry/registry-service/src/main/java/gov/nasa/pds/registry/resource/RegistryResource.java
@@ -47,75 +47,121 @@ import org.springframework.stereotype.Component;
 @Scope("request")
 public class RegistryResource {
 
-	@Context
-	UriInfo uriInfo;
+  @Context
+  UriInfo uriInfo;
 
-	@Context
-	Request request;
+  @Context
+  Request request;
 
-	@Autowired
-	RegistryService registryService;
+  @Autowired
+  RegistryService registryService;
 
-	/**
-	 * Retrieve the status of the registry service. This can be used to monitor
-	 * the health of the registry.
-	 * 
-	 * @response.representation.200.qname 
-	 *                                    {http://registry.pds.nasa.gov}status_information
-	 * @response.representation.200.mediaType application/xml
-	 * @response.representation.200.example {@link Examples#RESPONSE_STATUS}
-	 * 
-	 * @return registry status
-	 */
-	@GET
-	@Path("status")
-	@Produces( { MediaType.APPLICATION_XML, MediaType.TEXT_XML,
-			MediaType.APPLICATION_JSON })
-	public StatusInfo getStatus() {
-		return registryService.getStatus();
-	}
+  /**
+   * Retrieve the status of the registry service. This can be used to monitor
+   * the health of the registry.
+   * 
+   * @response.representation.200.qname 
+   *                                    {http://registry.pds.nasa.gov}status_information
+   * @response.representation.200.mediaType application/xml
+   * @response.representation.200.example {@link Examples#RESPONSE_STATUS}
+   * 
+   * @return registry status
+   */
+  @GET
+  @Path("status")
+  @Produces( { MediaType.APPLICATION_XML, MediaType.TEXT_XML,
+      MediaType.APPLICATION_JSON })
+  public StatusInfo getStatus() {
+    return registryService.getStatus();
+  }
 
-	/**
-	 * Synchronizes the incoming registry products with those already present in
-	 * the registry.
-	 * 
-	 * @param products
-	 *            from some other registry
-	 */
-	@PUT
-	@Path("sync")
-	@Consumes( { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	@Produces( { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public void synchronize(Collection<Product> products) {
-		// TODO implement
-	}
+  /**
+   * Synchronizes the incoming registry products with those already present in
+   * the registry.
+   * 
+   * @param products
+   *          from some other registry
+   */
+  @PUT
+  @Path("sync")
+  @Consumes( { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+  @Produces( { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+  public void synchronize(Collection<Product> products) {
+    // TODO implement
+  }
 
-	/**
-	 * Provides access to operations that can be done on products. The product
-	 * resource is simply a subresource of the registry resource.
-	 * 
-	 * @return product resource that will process the remaining portion of the
-	 *         request
-	 */
-	@Path("products")
-	public ProductsResource getProductResource() {
-		return new ProductsResource(uriInfo, request, this.registryService);
-	}
+  /**
+   * Provides access to operations that can be done on products. The product
+   * resource is simply a subresource of the registry resource.
+   * 
+   * @return product resource that will process the remaining portion of the
+   *         request
+   */
+  @Path("products")
+  public ProductsResource getProductsResource() {
+    return new ProductsResource(uriInfo, request, this.registryService);
+  }
 
-	/**
-	 * Provides access to operations that can be done on associations. The
-	 * association resource deals with links between registry objects. At this
-	 * level this is merely a method to be able to delegate along this resource
-	 * path.
-	 * 
-	 * @return association resource that will process the remaining portion of
-	 *         the request
-	 * 
-	 */
-	@Path("associations")
-	public AssociationResource getAssociationResource() {
-		return new AssociationResource(this.uriInfo, this.request,
-				this.registryService);
-	}
+  /**
+   * Provides access to operations that can be done on associations. The
+   * association resource deals with links between registry objects. At this
+   * level this is merely a method to be able to delegate along this resource
+   * path.
+   * 
+   * @return association resource that will process the remaining portion of the
+   *         request
+   * 
+   */
+  @Path("associations")
+  public AssociationsResource getAssociationsResource() {
+    return new AssociationsResource(this.uriInfo, this.request,
+        this.registryService);
+  }
 
+  /**
+   * Provides access to operations that can be done with Auditable Events.
+   * Auditable Events are recorded for many actions involved with
+   * RegistryObject's such as approved, created, deleted, deprecated, etc..
+   * 
+   * @return resource to deal with auditable events
+   */
+  @Path("events")
+  public EventsResource getEventsResource() {
+    return new EventsResource(this.uriInfo, this.request, this.registryService);
+  }
+
+  /**
+   * Provides access to operations that can be done with ClassificationSchemes.
+   * Additionally manages ClassificationNodes as each node can be traced back to
+   * a parent and should not exist without a scheme.
+   * 
+   * @return resource to deal with classification schemes
+   */
+  @Path("schemes")
+  public SchemesResource getSchemesResource() {
+    return new SchemesResource(this.uriInfo, this.request, this.registryService);
+  }
+
+  /**
+   * Provides access to operations that can be done with Service registrations.
+   * 
+   * @return resource to deal with services
+   */
+  @Path("services")
+  public ServicesResource getServicesResource() {
+    return new ServicesResource(this.uriInfo, this.request,
+        this.registryService);
+  }
+
+  /**
+   * Provides access to operations on Classifications that are made on
+   * RegistryObjects.
+   * 
+   * @return resource to deal with classifications
+   */
+  @Path("classifications")
+  public ClassificationsResource getClassificationsResource() {
+    return new ClassificationsResource(this.uriInfo, this.request,
+        this.registryService);
+  }
 }
