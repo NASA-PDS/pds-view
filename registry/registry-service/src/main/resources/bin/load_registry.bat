03d8ee2d6eb223802268d4aeb50e01df7115d464
shardman <shardman@45d5965c-88be-4637-9056-144873d6cd01>
2010-10-27 20:36:55 +0000

    Added blurb to the install document and added some headers to the scripts.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@7990 45d5965c-88be-4637-9056-144873d6cd01

--- registry/registry-service/src/main/resources/bin/load_registry.bat
+++ registry/registry-service/src/main/resources/bin/load_registry.bat
@@ -1,6 +1,22 @@
+:: Copyright 2010, by the California Institute of Technology.
+:: ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+:: Any commercial use must be negotiated with the Office of Technology Transfer
+:: at the California Institute of Technology.
+::
+:: This software is subject to U. S. export control laws and regulations
+:: (22 C.F.R. 120-130 and 15 C.F.R. 730-774). To the extent that the software
+:: is subject to U.S. export control laws and regulations, the recipient has
+:: the responsibility to obtain export licenses or other export authority as
+:: may be required before exporting such information to foreign countries or
+:: providing access to foreign nationals.
+::
+:: $Id$
+
+:: Batch file that registers the list of supported objects types 
+:: with the Registry Service specified by the REGISTRY_SERVICE variable.
 :: Upon successful completion of this script, go to 
-:: http://localhost:8080/registry-service/registry/status
-:: to verify that all classification nodes and schemes were loaded
+:: http://%REGISTRY_SERVICE%/registry/status
+:: to verify that all classification nodes and schemes were loaded.
 
 set REGISTRY_SERVICE=http://localhost:8080/registry-service
 
