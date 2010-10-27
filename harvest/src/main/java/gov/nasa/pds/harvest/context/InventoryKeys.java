863756284e4825be53950a314921f4c6990debe5
mcayanan <mcayanan@45d5965c-88be-4637-9056-144873d6cd01>
2010-10-27 21:30:30 +0000

    Class to hold metadata names that Harvest uses to support collections and bundles.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@8001 45d5965c-88be-4637-9056-144873d6cd01

---
+++ harvest/src/main/java/gov/nasa/pds/harvest/context/InventoryKeys.java
@@ -0,0 +1,6 @@
+package gov.nasa.pds.harvest.context;
+
+public interface InventoryKeys {
+    public static final String FILE_SPEC = "file_specification_name";
+    public static final String CHECKSUM = "md5_checksum";
+}
