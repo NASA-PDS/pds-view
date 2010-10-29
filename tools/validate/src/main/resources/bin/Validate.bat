73b7af106135a2ebc219a91945a25535a553fff4
mcayanan <mcayanan@45d5965c-88be-4637-9056-144873d6cd01>
2010-10-29 17:52:15 +0000

    Modify launch scripts to increase stack size in case validation occurs on a large number of files.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@8049 45d5965c-88be-4637-9056-144873d6cd01

--- tools/validate/src/main/resources/bin/Validate.bat
+++ tools/validate/src/main/resources/bin/Validate.bat
@@ -40,6 +40,6 @@ for %%i in ("%LIB_DIR%"\validate-*.jar) do set VALIDATE_JAR=%%i
 :: The special variable '%*' allows the arguments
 :: to be passed into the executable.
 
-java -jar "%VALIDATE_JAR%" %*
+java -Xss1024k -jar "%VALIDATE_JAR%" %*
 
 :END
