14d230daa4b760380310c2030ba2d7124837e3f8
shardman <shardman@45d5965c-88be-4637-9056-144873d6cd01>
2010-10-31 04:11:12 +0000

    Removed the -Xss entries since the stack overflow problem is now fixed in the core library.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@8085 45d5965c-88be-4637-9056-144873d6cd01

--- preparation/validate/src/main/resources/bin/Validate.bat
+++ preparation/validate/src/main/resources/bin/Validate.bat
@@ -40,6 +40,6 @@ for %%i in ("%LIB_DIR%"\validate-*.jar) do set VALIDATE_JAR=%%i
 :: The special variable '%*' allows the arguments
 :: to be passed into the executable.
 
-java -Xss128m -jar "%VALIDATE_JAR%" %*
+java -jar "%VALIDATE_JAR%" %*
 
 :END
