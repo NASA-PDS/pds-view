c603ff9a5fca9e4e2a05f7f9a7cf8b185d63a978
jpadams <jpadams@45d5965c-88be-4637-9056-144873d6cd01>
2011-01-13 22:50:52 +0000

    Removed target dir
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@8383 45d5965c-88be-4637-9056-144873d6cd01

--- report/transfer-logs/src/main/java/gov/nasa/pds/report/setup/servlets/SetupServlet.java
+++ report/transfer-logs/src/main/java/gov/nasa/pds/report/setup/servlets/SetupServlet.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.jcraft.jsch.Channel;
@@ -46,76 +45,76 @@ public class SetupServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	       JSch jsch = new JSch(); 
-	        Gson gson = new Gson();
-	        JsonObject root = new JsonObject();
-	        byte connect = 1;
-	        byte sftp = 1;
-	        String error = "";
-	        byte empty = 1;
-	       
-	       Session session = null;
-	       
-	       Host host = new Host();
-	       
-	       // Profile name to create/update
-	       host.setProfile(request.getParameter("profile"));
-	       // Remote machine host name or IP
-	       host.setHostname(request.getParameter("hostname"));
-	       // User name to connect the remote machine
-	       host.setUsername(request.getParameter("username"));
-	       // Password for remote machine authentication
-	       host.setPassword(request.getParameter("password"));    
-	       // Source file path on local machine
-	       //String srcPath = "D:\\Test.txt";
-	       // Destination directory location on remote machine
-	       host.setSrcPath(request.getParameter("srcPath"));
-	       // Regex to find files at source
-	       host.setRegex(request.getParameter("regex"));
-	       
-	       log.info("profile: "+host.getProfile());
-	       log.info("hostname: "+host.getHostname());
-	       log.info("username: "+host.getUsername());
-	       log.info("srcPath: "+host.getSrcPath());
-	       log.info("regex: "+host.getRegex());
-	       
-	       try {
-	        // Getting the session
-	        session = jsch.getSession(host.getUsername(), host.getHostname(), 22);
-	                    
-	        // Ignore HostKeyChecking
-	        session.setConfig("StrictHostKeyChecking", "no");                  
-	      
-	        // set the password for authentication
-	        session.setPassword(host.getPassword());
-	        session.connect();
-	                    
-	        // Getting the channel using sftp
-	        Channel channel = session.openChannel("sftp");
-	        channel.connect();
-	        ChannelSftp sftpChannel = (ChannelSftp) channel;
+       	JSch jsch = new JSch(); 
+        Gson gson = new Gson();
+        JsonObject root = new JsonObject();
+        byte connect = 1;
+        byte sftp = 1;
+        String error = "";
+        byte empty = 1;
+       
+       Session session = null;
+       
+       Host host = new Host();
+       
+       // Profile name to create/update
+       host.setProfile(request.getParameter("profile"));
+       // Remote machine host name or IP
+       host.setHostname(request.getParameter("hostname"));
+       // User name to connect the remote machine
+       host.setUsername(request.getParameter("username"));
+       // Password for remote machine authentication
+       host.setPassword(request.getParameter("password"));    
+       // Source file path on local machine
+       //String srcPath = "D:\\Test.txt";
+       // Destination directory location on remote machine
+       host.setSrcPath(request.getParameter("srcPath"));
+       // Regex to find files at source
+       host.setRegex(request.getParameter("regex"));
+       
+       log.info("profile: "+host.getProfile());
+       log.info("hostname: "+host.getHostname());
+       log.info("username: "+host.getUsername());
+       log.info("srcPath: "+host.getSrcPath());
+       log.info("regex: "+host.getRegex());
+       
+       try {
+        // Getting the session
+        session = jsch.getSession(host.getUsername(), host.getHostname(), 22);
+                    
+        // Ignore HostKeyChecking
+        session.setConfig("StrictHostKeyChecking", "no");                  
+      
+        // set the password for authentication
+        session.setPassword(host.getPassword());
+        session.connect();
+                    
+        // Getting the channel using sftp
+        Channel channel = session.openChannel("sftp");
+        channel.connect();
+        ChannelSftp sftpChannel = (ChannelSftp) channel;
 
-	        String[] dirList;
-	        String filename;
-	        Vector lsOut = sftpChannel.ls(host.getSrcPath());
-	        
-	        JsonArray matches = new JsonArray();
-	        for (Iterator it = lsOut.iterator(); it.hasNext();) {
-	        	dirList = it.next().toString().split(" ");
-	        	filename = dirList[dirList.length-1];
-	        	if (filename.matches(host.getRegex())) {
-	        		matches.add(new JsonPrimitive(filename));
-	        	}
-	        }
-	        
-	        if (!matches.isJsonNull()) {
-	        	empty = 0;
-	        	root.add("files", matches);
-	        }
-	        
-	        //root.add("count", new JsonPrimitive(matches.size()));
-	        
-	        // Found JSONLib has memory leak
+        String[] dirList;
+        String filename;
+        Vector lsOut = sftpChannel.ls(host.getSrcPath());
+        
+        JsonArray matches = new JsonArray();
+        for (Iterator it = lsOut.iterator(); it.hasNext();) {
+        	dirList = it.next().toString().split(" ");
+        	filename = dirList[dirList.length-1];
+        	if (filename.matches(host.getRegex())) {
+        		matches.add(new JsonPrimitive(filename));
+        	}
+        }
+        
+        if (!matches.isJsonNull()) {
+        	empty = 0;
+        	root.add("files", matches);
+        }
+        
+        //root.add("count", new JsonPrimitive(matches.size()));
+        
+        // Found JSONLib has memory leak
 //	        JSONObject jsonRoot = new JSONObject();
 //	        JSONArray fileList = new JSONArray();
 //	        if (!lsOut.isEmpty()) {
@@ -134,27 +133,27 @@ public class SetupServlet extends HttpServlet {
 //	        
 //	        PrintWriter out = response.getWriter();
 //	        out.print(jsonRoot.toString());
-	        
-	        // Exits the channel
-	         sftpChannel.exit();
-	                    
-	         // Disconnect the session
-	         session.disconnect();
+        
+        // Exits the channel
+         sftpChannel.exit();
+                    
+         // Disconnect the session
+         session.disconnect();
 
-	       } catch (JSchException e) {
-	    	   //root.add("connect", new JsonPrimitive(0));
-	    	   connect = 0;
-	    	   error = e.getMessage();
-	       } catch (SftpException e) {
-	           sftp = 0;
-	           error = e.getMessage();   
-	       } finally {
-		        root.add("connect", new JsonPrimitive(connect));
-		        root.add("sftp", new JsonPrimitive(sftp));
-		        root.add("error", new JsonPrimitive(error));
-		        root.add("empty", new JsonPrimitive(empty));
-	    	   gson.toJson(root, response.getWriter());
-	       }
+       } catch (JSchException e) {
+    	   //root.add("connect", new JsonPrimitive(0));
+    	   connect = 0;
+    	   error = e.getMessage();
+       } catch (SftpException e) {
+           sftp = 0;
+           error = e.getMessage();   
+       } finally {
+	        root.add("connect", new JsonPrimitive(connect));
+	        root.add("sftp", new JsonPrimitive(sftp));
+	        root.add("error", new JsonPrimitive(error));
+	        root.add("empty", new JsonPrimitive(empty));
+    	   gson.toJson(root, response.getWriter());
+       }
 	}
 
 }
