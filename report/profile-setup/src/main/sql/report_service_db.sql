8cac53c2ab09dbbf083eb73c331bbb84a28eb4e4
jpadams <jpadams@45d5965c-88be-4637-9056-144873d6cd01>
2011-03-17 20:37:06 +0000

    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@8703 45d5965c-88be-4637-9056-144873d6cd01

--- report/profile-setup/src/main/sql/report_service_db.sql
+++ report/profile-setup/src/main/sql/report_service_db.sql
@@ -23,16 +23,16 @@ DROP TABLE IF EXISTS `log_sets`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `log_sets` (
+  `log_set_id` int(11) NOT NULL AUTO_INCREMENT,
+  `set_number` int(11) NOT NULL,
+  `label` varchar(30) NOT NULL,
   `hostname` varchar(25) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(40) NOT NULL,
   `pathname` varchar(200) NOT NULL,
-  `created_by` varchar(30) DEFAULT NULL,
   `profile_id` int(11) NOT NULL,
-  `label` varchar(30) NOT NULL,
   `active_flag` varchar(1) DEFAULT 'y',
-  `log_set_id` int(11) NOT NULL AUTO_INCREMENT,
-  `set_number` int(11) NOT NULL,
+  `created_by` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`log_set_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -49,9 +49,9 @@ CREATE TABLE `profiles` (
   `node` varchar(3) NOT NULL,
   `identifier` varchar(20) NOT NULL,
   `name` varchar(25) NOT NULL,
-  `created_by` varchar(30) DEFAULT NULL,
   `method` varchar(10) NOT NULL,
   `active_flag` varchar(1) DEFAULT 'y',
+  `created_by` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`profile_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
