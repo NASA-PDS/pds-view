1af3a62e3575d4047b9f3ccd78c95e3989fe90fb
shardman <shardman@45d5965c-88be-4637-9056-144873d6cd01>
2011-03-24 22:16:00 +0000

    Updated the Design Tool documentation and added a site deploy script.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@8788 45d5965c-88be-4637-9056-144873d6cd01

---
+++ preparation/util/mvn-site-deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Copyright 2011, by the California Institute of Technology. 
+# ALL RIGHTS RESERVED. United States Government sponsorship acknowledged. 
+# Any commercial use must be negotiated with the Office of Technology Transfer 
+# at the California Institute of Technology. 
+#
+# This software is subject to U. S. export control laws and regulations 
+# (22 C.F.R. 120-130 and 15 C.F.R. 730-774). To the extent that the software 
+# is subject to U.S. export control laws and regulations, the recipient has 
+# the responsibility to obtain export licenses or other export authority as 
+# may be required before exporting such information to foreign countries or 
+# providing access to foreign nationals.
+#
+# $Id$
+
+# This script traverses the module directories to build and deploy
+# the associated sites to the repository.
+
+cd ..
+mvn clean
+mvn site
+
+cd core
+maven pdf
+cd ../design
+maven pdf
+cd ../validate
+maven pdf
+
+cd ..
+mvn site:deploy
