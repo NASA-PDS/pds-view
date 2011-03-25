6cbfc2622dd38cf0722f0519af05b3d92201dfcd
shardman <shardman@45d5965c-88be-4637-9056-144873d6cd01>
2011-03-25 21:38:16 +0000

    Updated with Windows CRLF line endings.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@8806 45d5965c-88be-4637-9056-144873d6cd01

--- preparation/validate/src/main/resources/bin/Validate.bat
+++ preparation/validate/src/main/resources/bin/Validate.bat
@@ -1,4 +1,4 @@
-:: Copyright 2008-2010, by the California Institute of Technology.
+:: Copyright 2008-2011, by the California Institute of Technology.
 :: ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
 :: Any commercial use must be negotiated with the Office of Technology Transfer
 :: at the California Institute of Technology.
