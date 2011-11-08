4e7d34cfbd9f3f14e5e7be5408ded518747e6fbe
shardman <shardman@45d5965c-88be-4637-9056-144873d6cd01>
2011-11-08 00:40:26 +0000

    A few more updates for consistency across the projects.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@9865 45d5965c-88be-4637-9056-144873d6cd01

--- preparation/generate/src/main/resources/bin/PDS4Generate.bat
+++ preparation/generate/src/main/resources/bin/PDS4Generate.bat
@@ -16,15 +16,22 @@
 :: without the need to set the CLASSPATH or having to type in that long java
 :: command (java gov.nasa.pds.pds4.generate.GenerateLauncher ...)
 
-@echo off
-
 :: Expects PDS4 Generate Tool jar file to be located in the ../lib directory.
 
+@echo off
+
+:: Set the JAVA_HOME environment variable here in the script if it will
+:: not be defined in the environment.
+if not defined JAVA_HOME (
+  set JAVA_HOME=\path\to\java\home
+)
+
+:: Setup environment variables.
 set SCRIPT_DIR=%~dps0
 set PARENT_DIR=%SCRIPT_DIR%..
-
 set LIB_DIR=%PARENT_DIR%\lib
 
+:: Check for dependencies.
 if exist "%LIB_DIR%\generate-*.jar" (
 set GENERATE_JAR=%LIB_DIR%\generate-*.jar
 ) else (
@@ -33,13 +40,11 @@ goto END
 )
 
 :: Finds the jar file in LIB_DIR and sets it to GENERATE_JAR
-
 for %%i in ("%LIB_DIR%"\generate-*.jar) do set GENERATE_JAR=%%i
 
 :: Executes GENERATE_TOOL via the executable jar file
 :: The special variable '%*' allows the arguments
 :: to be passed into the executable.
-
-java -jar "%GENERATE_JAR%" %*
+"%JAVA_HOME%"\bin\java -jar "%GENERATE_JAR%" %*
 
 :END
