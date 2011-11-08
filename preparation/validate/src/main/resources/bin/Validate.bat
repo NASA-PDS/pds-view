c73862318509a6cc0a1d01ce60c76b5198aec849
shardman <shardman@45d5965c-88be-4637-9056-144873d6cd01>
2011-11-08 00:43:08 +0000

    Bumped the version for the upcoming release and moved environment configuration from the Operation to Installation document.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@9866 45d5965c-88be-4637-9056-144873d6cd01

--- preparation/validate/src/main/resources/bin/Validate.bat
+++ preparation/validate/src/main/resources/bin/Validate.bat
@@ -1,4 +1,4 @@
-:: Copyright 2008-2011, by the California Institute of Technology.
+:: Copyright 2010-2011, by the California Institute of Technology.
 :: ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
 :: Any commercial use must be negotiated with the Office of Technology Transfer
 :: at the California Institute of Technology.
@@ -16,15 +16,22 @@
 :: without the need to set the CLASSAPTH or having to type in that long java
 :: command (java gov.nasa.pds.validate.ValidateLauncher ...)
 
-@echo off
-
 :: Expects the Validate Tool jar file to be located in the ../lib directory.
 
+@echo off
+
+:: Set the JAVA_HOME environment variable here in the script if it will
+:: not be defined in the environment.
+if not defined JAVA_HOME (
+  set JAVA_HOME=\path\to\java\home
+)
+
+:: Setup environment variables.
 set SCRIPT_DIR=%~dps0
 set PARENT_DIR=%SCRIPT_DIR%..
-
 set LIB_DIR=%PARENT_DIR%\lib
 
+:: Check for dependencies.
 if exist "%LIB_DIR%\validate-*.jar" (
 set VALIDATE_JAR=%LIB_DIR%\validate-*.jar
 ) else (
@@ -33,13 +40,11 @@ goto END
 )
 
 :: Finds the jar file in LIB_DIR and sets it to VALIDATE_JAR
-
 for %%i in ("%LIB_DIR%"\validate-*.jar) do set VALIDATE_JAR=%%i
 
 :: Executes the Validate Tool via the executable jar file
 :: The special variable '%*' allows the arguments
 :: to be passed into the executable.
-
-java -jar "%VALIDATE_JAR%" %*
+"%JAVA_HOME%"\bin\java -jar "%VALIDATE_JAR%" %*
 
 :END
