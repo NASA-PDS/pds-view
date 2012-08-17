f29c518433e1504066bb5867b02238d6bcfed414
jpadams <jpadams@45d5965c-88be-4637-9056-144873d6cd01>
2012-08-17 16:24:02 +0000

    Added facets dir to project.  Added TestConstants class.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@10873 45d5965c-88be-4637-9056-144873d6cd01

---
+++ search/search-core/src/test/gov/nasa/pds/search/core/constants/TestConstants.java
@@ -0,0 +1,12 @@
+package gov.nasa.pds.search.core.constants;
+
+public class TestConstants {
+	public static final String CORE_HOME="/Users/jpadams/Documents/workspace/2010-workspace/search/search-core/";
+	public static final String CONFIG_DIR_RELATIVE="./src/main/resources/conf/";
+	public static final String FACET_DIR_RELATIVE="./src/main/resources/facets/";
+	public static final String SERVICE_HOME_RELATIVE="./target/test";
+	public static final String PDS_REGISTRY_URL="http://pdsdev.jpl.nasa.gov:8080/registry";
+	public static final String PSA_REGISTRY_URL="http://ipda.jpl.nasa.gov/registry";
+	public static final String SOLR_SERVER_URL="http://pdsdev.jpl.nasa.gov:8080/search-service/pds" ;
+	
+}
