21088b0d7a17ab9cd5d77f34c9e5e67d524e7e2b
shardman <shardman@45d5965c-88be-4637-9056-144873d6cd01>
2012-09-25 18:19:13 +0000

    Source tree creation for the Transform Tool.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@10968 45d5965c-88be-4637-9056-144873d6cd01

---
+++ preparation/transform/README.txt
@@ -0,0 +1,16 @@
+The Transform Tool project contains software for transforming PDS3 and PDS4 
+products. The software is packaged in a JAR file.
+
+The software can be compiled with the "mvn compile" command but in order 
+to create the JAR file, you must execute the "mvn compile jar:jar" command. 
+The documentation including release notes, installation and operation of the 
+software should be online at 
+http://pds-cm.jpl.nasa.gov/2010/preparation/transform/. If it is not 
+accessible, you can execute the "mvn site:run" command and view the 
+documentation locally at http://localhost:8080.
+
+In order to create a complete distribution package, execute the 
+following commands: 
+
+% mvn site
+% mvn package
---
+++ preparation/transform/src/main/bin/transform.bat
@@ -0,0 +1,50 @@
+:: Copyright 2012, by the California Institute of Technology.
+:: ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+:: Any commercial use must be negotiated with the Office of Technology Transfer
+:: at the California Institute of Technology.
+::
+:: This software is subject to U. S. export control laws and regulations
+:: (22 C.F.R. 120-130 and 15 C.F.R. 730-774). To the extent that the software
+:: is subject to U.S. export control laws and regulations, the recipient has
+:: the responsibility to obtain export licenses or other export authority as
+:: may be required before exporting such information to foreign countries or
+:: providing access to foreign nationals.
+::
+:: $Id$
+
+:: Batch file that allows easy execution of the Transform Tool
+:: without the need to set the CLASSAPTH or having to type in that long java
+:: command (java gov.nasa.pds.transform.TransformLauncher ...)
+
+:: Expects the Transform Tool jar file to be located in the ../lib directory.
+
+@echo off
+
+:: Set the JAVA_HOME environment variable here in the script if it will
+:: not be defined in the environment.
+if not defined JAVA_HOME (
+  set JAVA_HOME=\path\to\java\home
+)
+
+:: Setup environment variables.
+set SCRIPT_DIR=%~dps0
+set PARENT_DIR=%SCRIPT_DIR%..
+set LIB_DIR=%PARENT_DIR%\lib
+
+:: Check for dependencies.
+if exist "%LIB_DIR%\validate-*.jar" (
+set TRANSFORM_JAR=%LIB_DIR%\transform-*.jar
+) else (
+echo Cannot find Transform Tool jar file in %LIB_DIR%
+goto END
+)
+
+:: Finds the jar file in LIB_DIR and sets it to TRANSFORM_JAR
+for %%i in ("%LIB_DIR%"\transform-*.jar) do set TRANSFORM_JAR=%%i
+
+:: Executes the Transform Tool via the executable jar file
+:: The special variable '%*' allows the arguments
+:: to be passed into the executable.
+"%JAVA_HOME%"\bin\java -jar "%TRANSFORM_JAR%" %*
+
+:END
