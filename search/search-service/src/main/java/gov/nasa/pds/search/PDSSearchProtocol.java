3bae3d1df90d717e6ecc3e47fb38534309e012a8
jpadams <jpadams@45d5965c-88be-4637-9056-144873d6cd01>
2013-03-25 22:24:59 +0000

    Updated PDSSearchProtocol to remove most of the parentheses from the query string. Apparently Solr does not like field-specific queries that are surrounded in parentheses. May require more research here, but for now this solution appears to work correctly, especially for basic queries.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@11481 45d5965c-88be-4637-9056-144873d6cd01

--- search/search-service/src/main/java/gov/nasa/pds/search/PDSSearchProtocol.java
+++ search/search-service/src/main/java/gov/nasa/pds/search/PDSSearchProtocol.java
@@ -1,4 +1,4 @@
-//	Copyright 2009-2010, by the California Institute of Technology.
+//	Copyright 2009-2013, by the California Institute of Technology.
 //	ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
 //	Any commercial use must be negotiated with the Office of Technology 
 //	Transfer at the California Institute of Technology.
@@ -24,157 +24,173 @@ import org.apache.solr.response.SolrQueryResponse;
 
 /**
  * @author pramirez
+ * @author jpadams
  * 
  */
 public class PDSSearchProtocol extends StandardRequestHandler {
-  private Logger LOG = Logger.getLogger(this.getClass().getName());
+	private Logger LOG = Logger.getLogger(this.getClass().getName());
 
-  public final static String[] MULTI_PARAMS = { "identifier", "instrument",
-      "instrument-host", "instrument-host-type", "instrument-type",
-      "investigation", "observing-system", "person", "product-class",
-      "target", "target-type", "title" };
-  public final static String QUERY_PARAM = "q";
-  public final static String START_TIME_PARAM = "start-time";
-  public final static String STOP_TIME_PARAM = "stop-time";
-  public final static String TERM_PARAM = "term";
-  public final static String ARCHIVE_STATUS_PARAM = "archive-status";
-  public final static String RETURN_TYPE_PARAM = "return-type";
-  public final static String VOTABLE = "votable";
+	public final static String[] MULTI_PARAMS = { "identifier", "instrument",
+			"instrument-host", "instrument-host-type", "instrument-type",
+			"investigation", "observing-system", "person", "product-class", "target",
+			"target-type", "title" };
+	public final static String QUERY_PARAM = "q";
+	public final static String START_TIME_PARAM = "start-time";
+	public final static String STOP_TIME_PARAM = "stop-time";
+	public final static String TERM_PARAM = "term";
+	public final static String ARCHIVE_STATUS_PARAM = "archive-status";
+	public final static String RETURN_TYPE_PARAM = "return-type";
+	public final static String VOTABLE = "votable";
 
-  @Override
-  public void handleRequestBody(SolrQueryRequest request,
-      SolrQueryResponse response) throws Exception {
-    ModifiableSolrParams pdsParams = new ModifiableSolrParams(request
-        .getParams());
-    request.setParams(pdsParams);
-    StringBuilder queryString = new StringBuilder();
+	@Override
+	public void handleRequestBody(SolrQueryRequest request,
+			SolrQueryResponse response) throws Exception {
+		ModifiableSolrParams pdsParams = new ModifiableSolrParams(
+				request.getParams());
+		request.setParams(pdsParams);
+		
+		/** Query string that will be returned as response **/
+		StringBuilder queryString = new StringBuilder();
 
-    // Handle multi valued parameters
-    for (String parameter : MULTI_PARAMS) {
-      if (request.getOriginalParams().getParams(parameter) != null) {
-        queryString.append("(");
-        // Loop through and add in the identifier to the query string
-        for (String value : request.getOriginalParams().getParams(parameter)) {
-          if (!value.trim().isEmpty()) {
-            queryString.append(parameter);
-            queryString.append(":");
-            queryString.append(value);
-            queryString.append(" OR ");
-          }
-        }
-        // Remove the last OR
-        queryString.delete(queryString.length() - 4, queryString.length());
-        queryString.append(")");
-        queryString.append(" AND ");
-      }
-    }
+		/** Count used to track the number of parameters specified in request **/
+		int multiParamCount = 0;
+		
+		int start = 0;
+		
+		// Handle multi valued parameters
+		for (String parameter : MULTI_PARAMS) {
+			if (request.getOriginalParams().getParams(parameter) != null) {
+				
+				start = queryString.length();
+				
+				// Loop through and add in the identifier to the query string
+				for (String value : request.getOriginalParams().getParams(parameter)) {
+					if (!value.trim().isEmpty()) {
+						queryString.append(parameter);
+						queryString.append(":");
+						queryString.append(value);
+						queryString.append(" OR ");
+					}
+				}
+				// Remove the last OR
+				queryString.delete(queryString.length() - 4, queryString.length());
 
-    // Remove the last AND
-    if (queryString.length() != 0) {
-      queryString.delete(queryString.length() - 5, queryString.length());
-    }
+				
+				if (request.getOriginalParams().getParams(parameter).length > 1) {
+					queryString.insert(start, "(");
+					queryString.append(")");
+				}
+				queryString.append(" AND ");
+				
+				multiParamCount++;
+			}
+		}
 
-    // Handle start time
-    if (request.getOriginalParams().getParams(START_TIME_PARAM) != null) {
-      // if there is already a portion of the query string group with AND
-      if (queryString.length() != 0) {
-        queryString.append(" AND ");
-      }
-      queryString.append(START_TIME_PARAM);
-      queryString.append(":");
-      queryString.append(request.getOriginalParams()
-          .getParams(START_TIME_PARAM)[0]);
-    }
+		this.LOG.info("Multi-param Query String pre-clean: " + queryString.toString());
+		
+		// Remove the last AND
+		if (queryString.length() != 0) {
+			if (multiParamCount == 1) { // Need to potentially remove parens from OR statement
+				this.LOG.info("Removing extra AND and ( ) from query string");
+				
+				queryString.delete(queryString.length() - 5, queryString.length());
+				
+				if (queryString.indexOf("(") != -1) {
+					queryString.deleteCharAt(queryString.indexOf(")"));
+					queryString.deleteCharAt(queryString.indexOf("("));
+				}
+				
+			} else {
+				this.LOG.info("Removing extra AND");
+				queryString.delete(queryString.length() - 5, queryString.length());
+			}
+		}
+		
+		this.LOG.info("Multi-param Query String post-clean: " + queryString.toString());
 
-    // Handle stop time
-    if (request.getOriginalParams().getParams(STOP_TIME_PARAM) != null) {
-      // if there is already a portion of the query string group with AND
-      if (queryString.length() != 0) {
-        queryString.append(" AND ");
-      }
-      queryString.append(STOP_TIME_PARAM);
-      queryString.append(":");
-      queryString
-          .append(request.getOriginalParams().getParams(STOP_TIME_PARAM)[0]);
-    }
+		// Handle start time
+		if (request.getOriginalParams().getParams(START_TIME_PARAM) != null) {
+			// if there is already a portion of the query string group with AND
+			if (queryString.length() != 0) {
+				queryString.append(" AND ");
+			}
+			queryString.append(START_TIME_PARAM);
+			queryString.append(":");
+			queryString.append(request.getOriginalParams()
+					.getParams(START_TIME_PARAM)[0]);
+		}
 
-    // JIRA Issue PDS-57
-    // Handle ignoring superseded data sets, if not specified in query
-    /*
-     * if (request.getOriginalParams().getParams(ARCHIVE_STATUS_PARAM) == null)
-     * { // Check if ARCHIVE_STATUS_PARAM is specified in queryString if
-     * (queryString.indexOf(ARCHIVE_STATUS_PARAM) == -1) {
-     * this.LOG.info(queryString.toString() + " -- indexOf - " +
-     * queryString.indexOf(ARCHIVE_STATUS_PARAM)); if (queryString.length() !=
-     * 0) { queryString.append(" AND "); } queryString.append("-");
-     * queryString.append(ARCHIVE_STATUS_PARAM);
-     * queryString.append(":SUPERSEDED"); } } else {
-     */
-    if (request.getOriginalParams().getParams(ARCHIVE_STATUS_PARAM) != null) {
-      // if there is already a portion of the query string group with AND
-      if (queryString.length() != 0) {
-        queryString.append(" AND ");
-      }
-      queryString.append(ARCHIVE_STATUS_PARAM);
-      queryString.append(":");
-      queryString.append(request.getOriginalParams().getParams(
-          ARCHIVE_STATUS_PARAM)[0]);
-    }
+		// Handle stop time
+		if (request.getOriginalParams().getParams(STOP_TIME_PARAM) != null) {
+			// if there is already a portion of the query string group with AND
+			if (queryString.length() != 0) {
+				queryString.append(" AND ");
+			}
+			queryString.append(STOP_TIME_PARAM);
+			queryString.append(":");
+			queryString.append(request.getOriginalParams().getParams(STOP_TIME_PARAM)[0]);
 
-    // Group other parameters together using ()
-    if (queryString.length() != 0) {
-      queryString.insert(0, "(");
-      queryString.append(")");
-    }
+			// Increment parameter counter
+			//andParamCount++;
+		}
 
-    // Handle terms
-    if (request.getOriginalParams().getParams(TERM_PARAM) != null) {
-      // if there is already a portion of the query string group with AND
-      if (queryString.length() != 0) {
-        queryString.append(" ");
-      }
-      queryString.append(request.getOriginalParams().getParams(TERM_PARAM)[0]);
-    }
+		if (request.getOriginalParams().getParams(ARCHIVE_STATUS_PARAM) != null) {
+			// if there is already a portion of the query string group with AND
+			if (queryString.length() != 0) {
+				queryString.append(" AND ");
+			}
+			queryString.append(ARCHIVE_STATUS_PARAM);
+			queryString.append(":");
+			queryString.append(request.getOriginalParams().getParams(
+					ARCHIVE_STATUS_PARAM)[0]);
+		}
 
-    // Handle query by appending to query string.
-    if (request.getOriginalParams().getParams(QUERY_PARAM) != null) {
-      // if there is already a portion of the query string group with AND
-      if (queryString.length() != 0) {
-        queryString.append(" ");
-      }
-      //queryString.append("(");
-      queryString.append(request.getOriginalParams().getParams(QUERY_PARAM)[0]);
-      //queryString.append(")");
-    }
+		// Handle terms
+		if (request.getOriginalParams().getParams(TERM_PARAM) != null) {
+			// if there is already a portion of the query string group with AND
+			if (queryString.length() != 0) {
+				queryString.append(" ");
+			}
+			queryString.append(request.getOriginalParams().getParams(TERM_PARAM)[0]);
+		}
 
-    // If there is a query pass it onto Solr as the q param
-    if (queryString.length() > 0) {
-      pdsParams.remove("q");
-      pdsParams.add("q", queryString.toString());
-    }
+		// Handle query by appending to query string.
+		if (request.getOriginalParams().getParams(QUERY_PARAM) != null) {
+			// if there is already a portion of the generic query string
+			if (queryString.length() != 0) {
+				queryString.append(" ");
+			}
+			queryString.append(request.getOriginalParams().getParams(QUERY_PARAM)[0]);
+		}
 
-    // Handle return type maps to Solrs wt param
-    if (request.getOriginalParams().getParams(RETURN_TYPE_PARAM) != null) {
-      String returnType = request.getOriginalParams().getParams(
-          RETURN_TYPE_PARAM)[0];
-      pdsParams.remove("wt");
-      if (VOTABLE.equals(returnType)) {
-        // Use Solr's Velocity Response Writer
-        pdsParams.add("wt", "velocity");
-        // Use votable.vm for the response style
-        pdsParams.add("v.layout", "votable");
-        // Use the results.vm to format the result set
-        pdsParams.add("v.template", "results");
-      } else {
-        // Just use Solr's default response writers
-        pdsParams.add("wt", request.getOriginalParams().getParams(
-            RETURN_TYPE_PARAM)[0]);
-      }
-    }
+		// If there is a query pass it onto Solr as the q param
+		if (queryString.length() > 0) {
+			pdsParams.remove("q");
+			pdsParams.add("q", queryString.toString());
+		}
 
-    this.LOG.info("Solr Query String: " + queryString);
+		// Handle return type maps to Solrs wt param
+		if (request.getOriginalParams().getParams(RETURN_TYPE_PARAM) != null) {
+			String returnType = request.getOriginalParams().getParams(
+					RETURN_TYPE_PARAM)[0];
+			pdsParams.remove("wt");
+			if (VOTABLE.equals(returnType)) {
+				// Use Solr's Velocity Response Writer
+				pdsParams.add("wt", "velocity");
+				// Use votable.vm for the response style
+				pdsParams.add("v.layout", "votable");
+				// Use the results.vm to format the result set
+				pdsParams.add("v.template", "results");
+			} else {
+				// Just use Solr's default response writers
+				pdsParams.add("wt",
+						request.getOriginalParams().getParams(RETURN_TYPE_PARAM)[0]);
+			}
+		}
 
-    super.handleRequestBody(request, response);
-  }
+		this.LOG.info("Solr Query String: " + queryString);
+
+		super.handleRequestBody(request, response);
+	}
 
 }
