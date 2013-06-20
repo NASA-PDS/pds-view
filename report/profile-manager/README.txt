e5b5608d1efc56c2979771fe3a5a1083a7626aca
jpadams <jpadams@45d5965c-88be-4637-9056-144873d6cd01>
2013-06-20 17:12:54 +0000

    Renamed profile-setup to be profile-manager.  Updated the servlets to remove the functionality for running sawmill after the logs are downloaded. Cleaned up the code a bit.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@11669 45d5965c-88be-4637-9056-144873d6cd01

---
+++ report/profile-manager/README.txt
@@ -0,0 +1,17 @@
+The Profile Setup is a user interface for configuring the Report Service. 
+The software is packaged in a WAR file.
+
+The software can be compiled with the "mvn compile" command but in order 
+to create the WAR file, you must execute the "mvn compile war:war" command. 
+
+The documentation including release notes, installation and operation of 
+the software should be online at 
+http://pds-cm.jpl.nasa.gov/pds4/report/profile-setup. 
+If it is not accessible, you can execute the "mvn site:run" command and 
+view the documentation locally at http://localhost:8080.
+
+In order to create a complete distribution package, execute the 
+following commands: 
+
+% mvn site
+% mvn package
---
+++ report/profile-manager/src/main/sql/profile_setup_db.sql
@@ -0,0 +1,68 @@
+-- MySQL dump 10.13  Distrib 5.5.8, for osx10.6 (i386)
+--
+-- Host: localhost    Database: profile_setup
+-- ------------------------------------------------------
+-- Server version	5.5.8
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `log_sets`
+--
+
+DROP TABLE IF EXISTS `log_sets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log_sets` (
+  `log_set_id` int(11) NOT NULL AUTO_INCREMENT,
+  `set_number` int(11) NOT NULL,
+  `label` varchar(30) NOT NULL,
+  `hostname` varchar(25) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `pathname` varchar(200) NOT NULL,
+  `profile_id` int(11) NOT NULL,
+  `active_flag` varchar(1) DEFAULT 'y',
+  `created_by` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`log_set_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `profiles`
+--
+
+DROP TABLE IF EXISTS `profiles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profiles` (
+  `profile_id` int(11) NOT NULL AUTO_INCREMENT,
+  `node` varchar(3) NOT NULL,
+  `identifier` varchar(20) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `method` varchar(10) NOT NULL,
+  `active_flag` varchar(1) DEFAULT 'y',
+  `created_by` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`profile_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-03-10 15:48:05
