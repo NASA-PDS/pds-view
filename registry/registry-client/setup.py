3df38674b9b9bfcc22cf14787746fc5201892d8b
kelly <kelly@45d5965c-88be-4637-9056-144873d6cd01>
2013-09-06 17:34:13 +0000

    bump default version
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@12010 45d5965c-88be-4637-9056-144873d6cd01

--- registry/registry-client/setup.py
+++ registry/registry-client/setup.py
@@ -38,7 +38,7 @@ if os.path.isfile(parentPOM):
     pomDoc = xml.dom.minidom.parse(os.path.join(os.path.dirname(__file__), '..', 'pom.xml'))
     _version = _valueFor('version', 'project', pomDoc)
 else:
-    _version = '1.2.0'
+    _version = '1.5.0'
 
 
 # Package data
