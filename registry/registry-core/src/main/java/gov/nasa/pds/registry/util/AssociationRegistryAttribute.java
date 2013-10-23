03bb67a63a915e24c87aedbc186b017d172b9e59
jpadams <jpadams@45d5965c-88be-4637-9056-144873d6cd01>
2013-10-23 18:15:02 +0000

    Check-in Part 2: Refactored the registry-core utility classes from the search-core into the registry-core. Renamed SearchCoreExtrinsic to ExtendedExtrinsicObject (could probably use a better name). Currently, all of these new classes are lumped together into the gov.nasa.pds.registry.util package. Attribute and Extrinsic Object extensions could probably be refactored into the client itself and/or some other areas of the code, but that is not a high priority item at the moment. Will create another ticket to further address this. refers to ticket PDS-199.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@12299 45d5965c-88be-4637-9056-144873d6cd01

---
+++ registry/registry-core/src/main/java/gov/nasa/pds/registry/util/AssociationRegistryAttribute.java
@@ -0,0 +1,65 @@
+package gov.nasa.pds.registry.util;
+
+import gov.nasa.pds.registry.query.AssociationFilter;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Provides the values for a given attribute specified in a product class
+ * configuration file.
+ * 
+ * @author jpadams
+ * @version $Revision: 11832 $
+ * 
+ */
+public enum AssociationRegistryAttribute {
+	/** Attribute for Logical Identifier. **/
+	ASSOCIATION_TYPE ("associationType") {
+		@Override
+		public AssociationFilter.Builder appendToFilterBuilder(AssociationFilter.Builder builder, String value) {
+			return builder.associationType(value);
+		}
+	},
+
+	/** Attribute for Product name. **/
+	SOURCE_OBJECT ("sourceObject") {
+		@Override
+		public AssociationFilter.Builder appendToFilterBuilder(AssociationFilter.Builder builder, String value) {
+			return builder.sourceObject(value);
+		}
+	},
+	
+	TARGET_OBJECT ("targetObject") {
+		@Override
+		public AssociationFilter.Builder appendToFilterBuilder(AssociationFilter.Builder builder, String value) {
+			return builder.sourceObject(value);
+		}
+	};
+
+	private String attributeName;
+    private static final Map<String, AssociationRegistryAttribute> lookup = new HashMap<String, AssociationRegistryAttribute>();
+
+	static {
+	    for(AssociationRegistryAttribute ra : EnumSet.allOf(AssociationRegistryAttribute.class))
+	         lookup.put(ra.getAttributeName(), ra);
+	}
+	
+	private AssociationRegistryAttribute(String attributeName) {
+		this.attributeName = attributeName;
+	}
+	
+	public String getAttributeName() {
+		return this.attributeName;
+	}
+	
+	public static AssociationRegistryAttribute get(String attributeName) {
+		return lookup.get(attributeName);
+	}
+	
+	public abstract AssociationFilter.Builder appendToFilterBuilder(AssociationFilter.Builder builder, String value);
+	
+	
+
+}
