ee09b3e86f9bcc8deb2ff48eb85e2c658944a0cd
mcayanan <mcayanan@45d5965c-88be-4637-9056-144873d6cd01>
2014-02-27 18:47:18 +0000

    Add copyright information.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@12756 45d5965c-88be-4637-9056-144873d6cd01

--- preparation/transform/src/main/java/gov/nasa/pds/transform/product/Pds3LabelTransformer.java
+++ preparation/transform/src/main/java/gov/nasa/pds/transform/product/Pds3LabelTransformer.java
@@ -1,3 +1,16 @@
+// Copyright 2006-2014, by the California Institute of Technology.
+// ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+// Any commercial use must be negotiated with the Office of Technology Transfer
+// at the California Institute of Technology.
+//
+// This software is subject to U. S. export control laws and regulations
+// (22 C.F.R. 120-130 and 15 C.F.R. 730-774). To the extent that the software
+// is subject to U.S. export control laws and regulations, the recipient has
+// the responsibility to obtain export licenses or other export authority as
+// may be required before exporting such information to foreign countries or
+// providing access to foreign nationals.
+//
+// $Id$
 package gov.nasa.pds.transform.product;
 
 import gov.nasa.pds.transform.TransformException;
@@ -8,6 +21,12 @@ import gov.nasa.pds.transform.util.Utility;
 import java.io.File;
 import java.util.logging.Logger;
 
+/**
+ * Class to support transformations given a PDS3 label.
+ *
+ * @author mcayanan
+ *
+ */
 public class Pds3LabelTransformer extends DefaultTransformer {
   /** logger object. */
   private static Logger log = Logger.getLogger(
