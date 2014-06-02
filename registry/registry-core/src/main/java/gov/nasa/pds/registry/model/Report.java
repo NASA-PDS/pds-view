17e0a8f6fbc5e8212fc73dff133ea9f028f1371c
shardman <shardman@45d5965c-88be-4637-9056-144873d6cd01>
2014-06-02 18:10:25 +0000

    Updates for harvest and the registry components for the 4.1.1 point build.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@13177 45d5965c-88be-4637-9056-144873d6cd01

--- registry/registry-core/src/main/java/gov/nasa/pds/registry/model/Report.java
+++ registry/registry-core/src/main/java/gov/nasa/pds/registry/model/Report.java
@@ -59,7 +59,7 @@ public class Report {
   private String home;
   
   @XmlAttribute
-  private String registryVersion = "1.6.0";
+  private String registryVersion = "1.6.1";
 
   public Report() {
     serverStarted = new Date();
