64a9e37514838408dc09746ed753b0ccf69ae38d
jpadams <jpadams@45d5965c-88be-4637-9056-144873d6cd01>
2014-06-09 20:34:02 +0000

    Add test constants.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@13206 45d5965c-88be-4637-9056-144873d6cd01

---
+++ registry/registry-core/src/test/gov/nasa/pds/registry/test/constants/TestConstants.java
@@ -0,0 +1,12 @@
+package gov.nasa.pds.registry.test.constants;
+
+import org.junit.Ignore;
+
+@Ignore
+public class TestConstants {
+
+	public static final String PDS3_REGISTRY_URL = "http://pds.jpl.nasa.gov/services/registry-pds3";
+	public static final String PDS4_REGISTRY_URL = "http://pds.jpl.nasa.gov/services/registry-pds4";
+	public static final String PSA_REGISTRY_URL = "http://planetarydata.org/registry";
+	
+}
