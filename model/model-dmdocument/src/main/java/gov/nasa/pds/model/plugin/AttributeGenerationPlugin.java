985d12a59d953a34767f9aa3570dfeb8a714655e
kelly <kelly@45d5965c-88be-4637-9056-144873d6cd01>
2014-06-20 17:10:12 +0000

    Add generators for classes and attributes
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@13230 45d5965c-88be-4637-9056-144873d6cd01

---
+++ model/model-dmdocument/src/main/java/gov/nasa/pds/model/plugin/AttributeGenerationPlugin.java
@@ -0,0 +1,34 @@
+//      Copyright 2014, by the California Institute of Technology.
+//      ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+//      Any commercial use must be negotiated with the Office of Technology 
+//      Transfer at the California Institute of Technology.
+//      
+//      This software is subject to U. S. export control laws and regulations 
+//      (22 C.F.R. 120-130 and 15 C.F.R. 730-774). To the extent that the software 
+//      is subject to U.S. export control laws and regulations, the recipient has 
+//      the responsibility to obtain export licenses or other export authority as 
+//      may be required before exporting such information to foreign countries or 
+//      providing access to foreign nationals.
+
+package gov.nasa.pds.model.plugin;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+* Attribute generation plugin. This is a Maven plugin to create lots of various
+* attributes from PDS ontology sources (which themselves come from Protégé).
+*/
+@Mojo(name="generateAttributes", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+public class AttributeGenerationPlugin extends AbstractGenerationPlugin {
+    protected void generateArtifacts() throws MojoExecutionException {
+        try {
+            DMDocument.main(new String[]{"-p", "-a"});
+        } catch (RuntimeException ex) {
+            throw ex;
+        } catch (Throwable ex) {
+            throw new MojoExecutionException("DMDocument error", ex);
+        }
+    }
+}
