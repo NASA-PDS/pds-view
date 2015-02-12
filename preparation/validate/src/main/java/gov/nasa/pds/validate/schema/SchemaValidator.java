d41e96c2087c080b7278d0fa3ba46a7124f9f7e6
mcayanan <mcayanan@45d5965c-88be-4637-9056-144873d6cd01>
2015-02-12 22:31:21 +0000

    Related to PDS-320: Provide support for handling import statements when validating a schema.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@13819 45d5965c-88be-4637-9056-144873d6cd01

--- preparation/validate/src/main/java/gov/nasa/pds/validate/schema/SchemaValidator.java
+++ preparation/validate/src/main/java/gov/nasa/pds/validate/schema/SchemaValidator.java
@@ -1,4 +1,4 @@
-// Copyright 2006-2014, by the California Institute of Technology.
+// Copyright 2006-2015, by the California Institute of Technology.
 // ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
 // Any commercial use must be negotiated with the Office of Technology Transfer
 // at the California Institute of Technology.
@@ -13,18 +13,29 @@
 // $Id$
 package gov.nasa.pds.validate.schema;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 
 import gov.nasa.pds.tools.label.ExceptionContainer;
 import gov.nasa.pds.tools.label.ExceptionType;
 import gov.nasa.pds.tools.label.LabelErrorHandler;
 import gov.nasa.pds.tools.label.LabelException;
+import gov.nasa.pds.tools.util.Utility;
 
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.SchemaFactory;
 
+import org.apache.commons.io.IOUtils;
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSResourceResolver;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -48,6 +59,163 @@ public class SchemaValidator {
   private EntityResolver resolver;
 
   /**
+   * Input Source implementation class.
+   *
+   * @author mcayanan
+   *
+   */
+  private class LSInputImpl implements LSInput {
+    private Reader characterStream;
+    private InputStream byteStream;
+    private String stringData;
+    private String systemId;
+    private String publicId;
+    private String baseURI;
+    private String encoding;
+    private boolean certifiedText;
+
+    @Override
+    public String getBaseURI() {
+      return baseURI;
+    }
+
+    @Override
+    public InputStream getByteStream() {
+      return byteStream;
+    }
+
+    @Override
+    public boolean getCertifiedText() {
+      return certifiedText;
+    }
+
+    @Override
+    public Reader getCharacterStream() {
+      return characterStream;
+    }
+
+    @Override
+    public String getEncoding() {
+      return encoding;
+    }
+
+    @Override
+    public String getPublicId() {
+      return publicId;
+    }
+
+    @Override
+    public String getStringData() {
+      return stringData;
+    }
+
+    @Override
+    public String getSystemId() {
+      return systemId;
+    }
+
+    @Override
+    public void setBaseURI(String uri) {
+      this.baseURI = uri;
+    }
+
+    @Override
+    public void setByteStream(InputStream input) {
+      this.byteStream = input;
+    }
+
+    @Override
+    public void setCertifiedText(boolean flag) {
+      this.certifiedText = flag;
+    }
+
+    @Override
+    public void setCharacterStream(Reader reader) {
+      this.characterStream = reader;
+    }
+
+    @Override
+    public void setEncoding(String encoding) {
+      this.encoding = encoding;
+    }
+
+    @Override
+    public void setPublicId(String publicId) {
+      this.publicId = publicId;
+    }
+
+    @Override
+    public void setStringData(String data) {
+      this.stringData = data;
+    }
+
+    @Override
+    public void setSystemId(String systemId) {
+      this.systemId = systemId;
+    }
+  }
+
+  /**
+   * Class that resolves import statements found within a schema file.
+   *
+   * @author mcayanan
+   *
+   */
+  private class SchemaResourceResolver implements LSResourceResolver {
+
+    /** Hashmap to hold the entities. */
+    private Map<String, byte[]> cachedEntities = new HashMap<String, byte[]>();
+
+    private ExceptionContainer container;
+
+    /**
+     * Constructor.
+     */
+    public SchemaResourceResolver(ExceptionContainer container) {
+      cachedEntities = new HashMap<String, byte[]>();
+      this.container = container;
+    }
+
+    @Override
+    public LSInput resolveResource(String type, String namespaceURI,
+        String publicId, String systemId, String baseURI) {
+
+      byte[] entity = cachedEntities.get(systemId);
+      LSInputImpl input = new LSInputImpl();
+      if (entity == null) {
+        InputStream in = null;
+        URLConnection conn = null;
+        try {
+          URL base = new URL(baseURI);
+          base = base.toURI().getPath().endsWith("/") ?
+              base.toURI().resolve("..").toURL() :
+                base.toURI().resolve(".").toURL();
+          URL url = new URL(base, systemId);
+          conn = url.openConnection();
+          in = Utility.openConnection(conn);
+          entity = IOUtils.toByteArray(in);
+          cachedEntities.put(systemId, entity);
+        } catch (Exception e) {
+          container.addException(new LabelException(ExceptionType.FATAL,
+              e.getMessage(), systemId, systemId, null, null));
+        } finally {
+          IOUtils.closeQuietly(in);
+          IOUtils.close(conn);
+        }
+      }
+      input.setPublicId(publicId);
+      input.setSystemId(systemId);
+      input.setBaseURI(baseURI);
+      input.setCharacterStream(
+          new InputStreamReader(new ByteArrayInputStream(entity)));
+
+      return input;
+    }
+  }
+
+
+
+  /**
    * Constructor.
    *
    * @param resolver An entity resolver.
@@ -69,8 +237,8 @@ public class SchemaValidator {
    */
   public ExceptionContainer validate(URL schema) {
     ExceptionContainer container = new ExceptionContainer();
-    // Fix validate label method to always set the
     schemaFactory.setErrorHandler(new LabelErrorHandler(container));
+    schemaFactory.setResourceResolver(new SchemaResourceResolver(container));
     try {
       InputSource inputSource = resolver.resolveEntity("", schema.toString());
       StreamSource source = new StreamSource(inputSource.getByteStream());
