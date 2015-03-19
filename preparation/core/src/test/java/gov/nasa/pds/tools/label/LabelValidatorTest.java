7a39f6cc716dab164c5d8db948ea9c0c0fe2bbdf
mrose <mrose@45d5965c-88be-4637-9056-144873d6cd01>
2015-03-19 23:29:43 +0000

    Ensured that XML Schema validation messages contain the proper source line number.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@13883 45d5965c-88be-4637-9056-144873d6cd01

--- preparation/core/src/test/java/gov/nasa/pds/tools/label/LabelValidatorTest.java
+++ preparation/core/src/test/java/gov/nasa/pds/tools/label/LabelValidatorTest.java
@@ -2,6 +2,7 @@ package gov.nasa.pds.tools.label;
 
 import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 import java.io.File;
@@ -111,6 +112,47 @@ public class LabelValidatorTest {
     for (LabelException exception : container.getExceptions()) {
       if (exception.getExceptionType() == ExceptionType.WARNING) {
         assertThat(exception.getLineNumber(), is(126));
+      }
+    }
+  }
+
+  // Test that XML validation messages have source locations.
+  @Test
+  public void testXMLValidationHasLocation() throws IOException, MalformedURLException, SAXException, ParserConfigurationException, TransformerException, MissingLabelSchemaException {
+    LabelValidator validator = new LabelValidator();
+    ExceptionContainer container = new ExceptionContainer();
+    validator.setSchema(Collections.singletonList(new File("src/test/resources/PDS4_PDS_1301.xsd").toURI().toURL()));
+    validator.setSchematrons(Collections.singletonList(pds4Schematron));
+    validator.validate(container, new File("src/test/resources/ele_mom_tblChar.xml"));
+    showExceptions(container);
+    assertThat(container.hasFatal(), is(false));
+    assertThat(container.hasError(), is(true));
+    assertThat(container.hasWarning(), is(false));
+
+    for (LabelException exception : container.getExceptions()) {
+      if (exception.getExceptionType()==ExceptionType.ERROR && exception.getMessage().contains("matching wildcard")) {
+        assertThat(exception.getLineNumber(), not(is(-1)));
+      }
+    }
+  }
+
+  // Test that errors on files containing CDATA sections and XML
+  // entities have source locations.
+  @Test
+  public void testXMLSpecialConstructs() throws IOException, MalformedURLException, SAXException, ParserConfigurationException, TransformerException, MissingLabelSchemaException {
+    LabelValidator validator = new LabelValidator();
+    ExceptionContainer container = new ExceptionContainer();
+    validator.setSchema(Collections.singletonList(new File("src/test/resources/PDS4_PDS_1301.xsd").toURI().toURL()));
+    validator.setSchematrons(Collections.singletonList(pds4Schematron));
+    validator.validate(container, new File("src/test/resources/ProductWithSpecialXML.xml"));
+    showExceptions(container);
+    assertThat(container.hasFatal(), is(false));
+    assertThat(container.hasError(), is(true));
+    assertThat(container.hasWarning(), is(true));
+
+    for (LabelException exception : container.getExceptions()) {
+      if (exception.getExceptionType()==ExceptionType.ERROR) {
+        assertThat(exception.getLineNumber(), not(is(-1)));
       }
     }
   }
