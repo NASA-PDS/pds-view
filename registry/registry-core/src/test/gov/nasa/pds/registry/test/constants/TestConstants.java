7eee4e645bbef8b2cd332cfb819883d779a54544
jpadams <jpadams@45d5965c-88be-4637-9056-144873d6cd01>
2015-04-09 22:28:33 +0000

    update test constants to use https instead of http
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@13971 45d5965c-88be-4637-9056-144873d6cd01

--- registry/registry-core/src/test/gov/nasa/pds/registry/test/constants/TestConstants.java
+++ registry/registry-core/src/test/gov/nasa/pds/registry/test/constants/TestConstants.java
@@ -5,8 +5,8 @@ import org.junit.Ignore;
 @Ignore
 public class TestConstants {
 
-	public static final String PDS3_REGISTRY_URL = "http://pds.jpl.nasa.gov/services/registry-pds3";
-	public static final String PDS4_REGISTRY_URL = "http://pds.jpl.nasa.gov/services/registry-pds4";
+	public static final String PDS3_REGISTRY_URL = "https://pds.jpl.nasa.gov/services/registry-pds3";
+	public static final String PDS4_REGISTRY_URL = "https://pds.jpl.nasa.gov/services/registry-pds4";
 	public static final String PSA_REGISTRY_URL = "http://planetarydata.org/registry";
 	
 }
