5585e0346b3217f297d69458f286e82132f5a615
jpadams <jpadams@45d5965c-88be-4637-9056-144873d6cd01>
2015-07-02 22:06:26 +0000

    quick script to run on personal machine to prepare metrics to submit to Emily. includes pulling the HQ reports from pds-rpt and renaming for usa/intl and adding the data.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@14159 45d5965c-88be-4637-9056-144873d6cd01

---
+++ report/report-manager/src/main/resources/bin/generate_metrics.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "enter date YYYY-MM"
+    exit
+fi
+
+date=$1
+url=pds-rpt
+REP_HOME=/report_service/reports
+NODES="en img rings ppi rings sbn_umd sbn_psi naif geo"
+
+mkdir -p $date
+rsync -av pds-rpt:${REP_HOME}/*/${date}/*_hq_domain_report_*.csv ${date}/
+cd ${date}
+for node in $NODES; do
+    if [ -f ${node}_hq_domain_report_0.csv ]; then
+	mv ${node}_hq_domain_report_0.csv ${node}_hq_domain_report_usa_${date}.csv
+	open ${node}_hq_domain_report_usa_${date}.csv
+	mv ${node}_hq_domain_report_1.csv ${node}_hq_domain_report_intl_${date}.csv
+	open ${node}_hq_domain_report_intl_${date}.csv
+    fi
+done
+
+exit 0
