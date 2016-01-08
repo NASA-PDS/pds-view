ef1f2be93b3272d86f4c417a248f077a9b4c2ed3
earevalo <earevalo@45d5965c-88be-4637-9056-144873d6cd01>
2016-01-08 21:14:19 +0000

    Fixed Data Dictionary Occasional Incorrect Definition
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@14514 45d5965c-88be-4637-9056-144873d6cd01

--- portal/dd-search/src/main/webapp/widgets/ResultWidget.js
+++ portal/dd-search/src/main/webapp/widgets/ResultWidget.js
@@ -2,6 +2,7 @@
 	AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
 		self : null,
 		lastRequestNumber:0,
+		docs: null,
 
 		init: function () {
 			self = this;
@@ -27,12 +28,14 @@
 
 		afterRequest: function(){
 			var requestNumber = this.manager.response.responseHeader.params["_"];
+			console.log("requestNumber", requestNumber);
+			console.log("response", this.manager.response.response.docs);
 
 			//Compare the request numbers. The highest is the latest search result.
 			if(parseInt(requestNumber, 10) > this.lastRequestNumber){
 				this.lastRequestNumber = requestNumber;
+				this.docs = this.manager.response.response.docs;
 
-				console.log("response:", this.manager.response.response.docs);
 				$(this.target).empty();
 				this.setResultHeaders($(this.target));
 				for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++){
@@ -40,6 +43,9 @@
 					$(this.target).append(this.template(doc, i));
 				}
 				$(this.target).fadeIn();
+			}
+			else{
+				this.manager.response.response.docs = this.docs;
 			}
 		},
 
