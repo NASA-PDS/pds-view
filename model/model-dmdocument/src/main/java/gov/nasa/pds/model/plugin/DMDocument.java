99cd4a399e7d81ad3ccd11b132c117de53b5225d
jshughes <jshughes@45d5965c-88be-4637-9056-144873d6cd01>
2016-09-28 23:20:16 +0000

    V1.7.0.0 (9/28/16) Data file submitted to Repository
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@15165 45d5965c-88be-4637-9056-144873d6cd01

--- model/model-dmdocument/src/main/java/gov/nasa/pds/model/plugin/DMDocument.java
+++ model/model-dmdocument/src/main/java/gov/nasa/pds/model/plugin/DMDocument.java
@@ -262,8 +262,8 @@ public class DMDocument extends Object {
 		masterClassOrder = 1000;
 
 		// master group number
-//		masterClassOrder = 10;
-		masterGroupNum = 10;
+		masterClassOrder = 10;
+//		masterGroupNum = 10;
 		
 		omitClass = new ArrayList <String> ();
 		omitClass.add("Data_Object");
