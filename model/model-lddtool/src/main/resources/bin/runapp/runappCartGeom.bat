61da8a7275d2a84f8b5eb31da8f1878f2785d08a
jshughes <jshughes@45d5965c-88be-4637-9056-144873d6cd01>
2016-10-12 17:57:53 +0000

    Remove example user RUN script from /bin directory.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@15272 45d5965c-88be-4637-9056-144873d6cd01

--- model/model-lddtool/src/main/resources/bin/runapp/runappCartGeom.bat
+++
@@ -1,7 +0,0 @@
-Rem ---------------------------------
-Rem   Begin LDD Tool
-Rem ---------------------------------
-.\bin\lddtool.bat -lpmcJ1 IngestLDDTool.xml > list.txt
-Rem -------------------------------
-Rem   End LDD Tool
-Rem -------------------------------
--- model/model-lddtool/src/main/resources/bin/runapp/runapp_MultiTest.bat
+++
@@ -1,7 +0,0 @@
-Rem ---------------------------------
-Rem   Begin LDD Tool
-Rem ---------------------------------
-.\bin\lddtool.bat -lpmcJ1 IngestLDDTool.xml IngestLDDTool1.xml IngestLDDTool2.xml IngestLDDTool3.xml > list.txt
-Rem -------------------------------
-Rem   End LDD Tool
-Rem -------------------------------
