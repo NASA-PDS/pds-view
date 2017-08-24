521f81a9dcbde2d6bc93b4fa5658b2b3c76f3527
jpadams <jpadams@45d5965c-88be-4637-9056-144873d6cd01>
2017-08-24 23:46:43 +0000

    add capitalize method to be more smart about capitalizing objects versus strings.
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@15933 45d5965c-88be-4637-9056-144873d6cd01

--- preparation/generate/src/main/java/gov/nasa/pds/imaging/generate/util/TextUtil.java
+++ preparation/generate/src/main/java/gov/nasa/pds/imaging/generate/util/TextUtil.java
@@ -22,4 +22,16 @@ public class TextUtil extends WordUtils implements PDSContext {
 		str = str.replace('_', ' ');
 		return WordUtils.capitalizeFully(str);
 	}
+	
+    /**
+     * Utility method used to transform the object into the applicable
+     * PDS title-case format for enumerated values.
+     * 
+     * @param obj
+     * @return
+     */
+    public static String capitalize(Object obj) {
+        String str = obj.toString().replace('_', ' ');
+        return WordUtils.capitalizeFully(str);
+    }
 }
