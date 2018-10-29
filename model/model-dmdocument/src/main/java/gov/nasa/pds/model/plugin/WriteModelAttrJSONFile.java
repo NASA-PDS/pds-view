9f11dbf16cbe20bd725abd9e4037d798c65be8e9
jshughes <jshughes@45d5965c-88be-4637-9056-144873d6cd01>
2018-10-29 14:33:51 +0000

    Stub for new routine
    git-svn-id: https://starcell.jpl.nasa.gov/repo/2010/trunk@17134 45d5965c-88be-4637-9056-144873d6cd01

---
+++ model/model-dmdocument/src/main/java/gov/nasa/pds/model/plugin/WriteModelAttrJSONFile.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Writes the PDS4 DD content to a JSON file - Stub
+ *   
+ */
+
+class WriteModelAttrJSONFile extends Object{
+	
+
+	public WriteModelAttrJSONFile () {
+		return;
+	}
+
+	// write the JSON file
+	public void writeJSONFile (SchemaFileDefn lSchemaFileDefn) throws java.io.IOException {
+		return;
+	}
+}	
